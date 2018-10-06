31ec06220855409e606a366f1cd282f717c2dbd3
RednaD <russo.alice.42@gmail.com>
2018-10-06 15:51:07 +0200

    Best Fractol ever. Lol.

---
+++ srcs/draw.c
@@ -0,0 +1,111 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   draw.c                                             :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: arusso <marvin@42.fr>                      +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2018/10/01 14:44:03 by arusso            #+#    #+#             */
+/*   Updated: 2018/10/06 15:36:57 by arusso           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "../includes/fractol.h"
+
+static int	get_val_bur(long double x1, long double y1, t_global *g)
+{
+	double		r;
+	double		im;
+	double		t;
+	long int	it;
+
+	r = 0;
+	im = 0;
+	it = 0;
+	while ((r * r + im * im) < 4 && it < g->it_max)
+	{
+		t = r * r - im * im + x1;
+		im = fabsl((double)(2 * r * im) + y1);
+		r = fabsl((double)(t));
+		it++;
+	}
+	return (it);
+}
+
+static int	get_val_mandelia(long double x1, long double y1, t_global *g)
+{
+	long double	r;
+	long double	ri;
+	long double	t;
+	long double	it;
+
+	it = 0;
+	r = 0;
+	ri = 0;
+	if (ft_strequ(g->name, "julia"))
+	{
+		r = x1;
+		ri = y1;
+		x1 = g->v1;
+		y1 = g->v2;
+	}
+	while ((r * r + ri * ri) < 4 && it < g->it_max)
+	{
+		t = r;
+		r = (r * r) - (ri * ri) + x1;
+		ri = ((2 * ri) * t) + y1;
+		it++;
+	}
+	return (it);
+}
+
+static int	get_thread_id(pthread_t id, pthread_t *thread)
+{
+	int i;
+
+	i = 0;
+	while (i < THREAD && !pthread_equal(id, thread[i]))
+		i++;
+	return (i);
+}
+
+static void	*draw_thread(void *g_data)
+{
+	t_global *g;
+	t_local	l;
+
+	g = (t_global*)g_data;
+	l.padding = WIDTH / THREAD;
+	l.x = get_thread_id(pthread_self(), g->thread) * l.padding;
+	l.x_end = l.x + l.padding + 1;
+	while (l.x < g->max_y && l.x < l.x_end)
+	{
+		l.y = 0.0;
+		while (l.y < g->max_y)
+		{
+			l.x1 = (l.x - g->rv1) / g->zoom;
+			l.y1 = (l.y - g->rv2) / g->zoom;
+			if (ft_strequ(g->name, "julia") || ft_strequ(g->name, "mandelbrot"))
+				l.val = get_val_mandelia(l.x1, l.y1, g);
+			else
+				l.val = get_val_bur(l.x1, l.y1, g);
+			((int*)g->data)[(int)(l.x + l.y * WIDTH)] = g->color * l.val;
+			l.y++;
+		}
+		l.x++;
+	}
+	pthread_exit(NULL);
+}
+
+void		draw(t_global *g)
+{
+	int		i;
+
+	i = -1;
+	while (++i < THREAD)
+		pthread_create(&g->thread[i], NULL, draw_thread, g);
+	i = -1;
+	while (++i < THREAD)
+		pthread_join(g->thread[i], NULL);
+	mlx_put_image_to_window(g->mlx, g->win, g->img, 0, 0);
+}
---
+++ srcs/main.c
@@ -0,0 +1,76 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.c                                             :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: arusso <marvin@42.fr>                      +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2018/09/15 15:28:03 by arusso            #+#    #+#             */
+/*   Updated: 2018/09/27 16:05:44 by arusso           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "../includes/fractol.h"
+
+void	put_controls(char *str)
+{
+	ft_putendl("Controls for Fract'ol :");
+	ft_putendl("	- Move fractal with arrows ;");
+	ft_putendl("	- Add or remove iterations with '*' and '/' ; ");
+	ft_putendl("	- Zoom with left click and mouse wheel forward ; ");
+	ft_putendl("	- Zoom out with right click and mouse wheel backward ; ");
+	if (ft_strequ(str, "julia"))
+	{
+		ft_putendl("	- Change fractal shape moving your mouse ;");
+		ft_putendl("	- Freeze and unfreeze fractal with space bar ;");
+	}
+	ft_putendl("	- Change color with the numeric keypad from 0 to 9 ;");
+	ft_putendl("	- Reset fractal with 'r' ;");
+	ft_putendl("	- Exit with 'ESC'.");
+}
+
+void	get_start_values(t_global *g)
+{
+	g->color = START_COLOR;
+	g->move = 0;
+	g->max_x = WIDTH;
+	g->max_y = HEIGHT;
+	g->rv1 = WIDTH / 2;
+	g->rv2 = HEIGHT / 2;
+	g->nb_zoom = 1;
+	g->v1 = JULIA_R;
+	g->v2 = JULIA_IM;
+	g->it_max = 50;
+	g->zoom = 250;
+}
+
+void	init_global(t_global *g)
+{
+	g->mlx = mlx_init();
+	g->win = mlx_new_window(g->mlx, WIDTH, HEIGHT, "Fract'ol");
+	g->img = mlx_new_image(g->mlx, WIDTH, HEIGHT);
+	g->data = (unsigned int*)mlx_get_data_addr(g->img,
+			&g->bpp, &g->line, &g->endian);
+	get_start_values(g);
+	put_controls(g->name);
+}
+
+int		main(int ac, char **av)
+{
+	t_global	*g;
+
+	if (ac != 2 || (ft_strequ(av[1], "mandelbrot") + ft_strequ(av[1], "julia")
+				+ ft_strequ(av[1], "burningship")) == 0)
+		ft_error("Avalaible fractals : mandelbrot, julia, burningship.");
+	if (!(g = (t_global*)malloc(sizeof(t_global))))
+		ft_error("Malloc failed.");
+	g->name = av[1];
+	init_global(g);
+	draw(g);
+	mlx_hook(g->win, 2, (1L << 0), key_handle, g);
+	mlx_mouse_hook(g->win, zoom, g);
+	if (ft_strequ(g->name, "julia") && g->move == 0)
+		mlx_hook(g->win, 6, 3, key_move, g);
+	mlx_loop(g->mlx);
+	return (0);
+}
